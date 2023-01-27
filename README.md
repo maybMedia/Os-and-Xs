94786c7c4015bac11237aebc555d6a29e849db42
maybMedia <braithmay@yahoo.com.au>
2023-01-28 01:37:39 +1100

    First iteration of the game. Updated README.

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Noughts and Crosses
-### Programmed in python
+
+## Process
+1. I began by creating a simple board and storing it in four variables; row1, row2, row3 and break1.
+2. I then created a simple loop to check if the game is in progress or ready to start which also included some instructions.
+3. I then created a loop which printed the board and who's turn it is to the console each turn and requested an input from the user. This input is then interpreted and a row and column is given to the value.
+4. I then created a function which checks if there is already a symbol in that row and column. I feel like I have not elegantly implemented this function as it uses 3 if statements to determine the row to check.
+5. Then I created a function which if the row and column are empty, replaces the blank space with the symbol of the current player.
+
+## Screenshots
+
+
+## Tools Used
+
+
+## Skills learnt or improved
+- I became more comfortable with using functions.
+- I improved my understanding of converting strings to lists and joining them back together.
+
+## Challenges
+- I really struggled with coming up with a method to check whether the position was filled or not. I solved this by using a dictionary with a value for the position and then the index which matched that in the split up string.
---
+++ main.py
@@ -0,0 +1,120 @@
+import time
+
+row1 = "   :   :   "
+row2 = "   :   :   "
+row3 = "   :   :   "
+break1 = "--- --- ---"
+started = False
+symbol = 'X'
+key = {1 : 1, 2 : 5, 3 : 9}
+turns = 0
+
+def returnBoard(row1, row2, row3, break1):
+    board = f"{row1}\n{break1}\n{row2}\n{break1}\n{row3}"
+    return board
+
+def findColumn(instruction):
+    if instruction == 1 or instruction == 4 or instruction == 7:
+        return 1
+    elif instruction == 2 or instruction == 5 or instruction == 6:
+        return 2
+    else:
+        return 3
+
+def findRow(instruction):
+    if instruction > 0 and instruction <= 3:
+        return 1
+    elif instruction >= 4 and instruction <= 6:
+        return 2
+    else:
+        return 3
+
+def checkIfEmpty(row, column, row1, row2, row3, key):
+    index = key[column]
+    if row == 1:
+        row1 = list(row1)
+        if row1[index] == " ":
+            return True
+        else:
+            return False
+
+    elif row == 2:
+        row2 = list(row2)
+        if row2[index] == " ":
+            return True
+        else:
+            return False
+
+    else:
+        row3 = list(row3)
+        if row3[index] == " ":
+            return True
+        else:
+            return False
+
+def updateRow(column, row, symbol, row1, row2, row3):
+    if row == 1:
+        row1 = list(row1)
+        row1[key[column]] = symbol
+        row1 = ''.join(row1)
+        return row1
+    elif row == 2:
+        row2 = list(row2)
+        row2[key[column]] = symbol
+        row2 = ''.join(row2)
+        return row2
+    else:
+        row3 = list(row3)
+        row3[key[column]] = symbol
+        row3 = ''.join(row3)
+        return row3
+
+def checkForWin(symbol, row1, row2, row3):
+    return False
+
+print("Are you ready to play a game of noughts and crosses?\nReset the board at any time by typing 'reset'!\nType 'yes' to start!")
+while started == False:
+    start = input('')
+    if start == 'yes':
+        started = True
+
+while started == True:
+    print(f"It's {symbol}'s turn!")
+    print(returnBoard(row1, row2, row3, break1))
+    instruction = input(f"Where would you like to place your {symbol}? [Position 1-9]\n")
+    if instruction != 'reset':
+        instruction = int(instruction)
+        if instruction <= 9 and instruction >= 1:
+            column = findColumn(instruction)
+            row = findRow(instruction)
+            if checkIfEmpty(row, column, row1, row2, row3, key) == True:
+                if row == 1:
+                    row1 = updateRow(column, row, symbol, row1, row2, row3)
+                elif row == 2:
+                    row2 = updateRow(column, row, symbol, row1, row2, row3)
+                else:
+                    row3 = updateRow(column, row, symbol, row1, row2, row3)
+                if symbol == 'X':
+                    symbol = 'O'
+                else:
+                    symbol = 'X'
+                turns = turns + 1
+                if turns >= 5:
+                    if checkForWin() == True:
+                        print(f"{symbol} Wins!")
+                        time.sleep(1)
+                        print('Resetting...')
+                        time.sleep(5)
+            else:
+                print("There is already an X or an O in that position!")
+        else:
+            print("Sorry I didn't understand that instruction. I was expecting a number(1-9), the positions on the board are numbered as follows; 1 is top left, 2 is top middle etc.")
+    else:
+        print("Resetting...")
+        time.sleep(3)
+        started = False
+        row1 = "   :   :   "
+        row2 = "   :   :   "
+        row3 = "   :   :   "
+        symbol = 'X'
+        turns = 0
